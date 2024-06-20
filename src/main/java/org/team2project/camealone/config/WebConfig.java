63997e1fa237a1de7340013f2981bb7c2e194075
LoadingV4 <89256027+LoadingV4@users.noreply.github.com>
2024-06-20 11:35:27 +0900

    Update WebConfig.java

--- src/main/java/org/team2project/camealone/config/WebConfig.java
+++ src/main/java/org/team2project/camealone/config/WebConfig.java
@@ -3,11 +3,13 @@ package org.team2project.camealone.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
