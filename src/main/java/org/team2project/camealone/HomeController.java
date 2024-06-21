2d5298d1ec96b81d901e387e7fb845d7fffd7879
LoadingV4 <89256027+LoadingV4@users.noreply.github.com>
2024-06-21 11:14:24 +0900

    Add files via upload

---
+++ src/main/java/org/team2project/camealone/HomeController.java
@@ -0,0 +1,47 @@
+package org.team2project.camealone;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/") //  메인페이지
+    public String home(Model model,HttpSession session) {
+        String id=(String) session.getAttribute("id");
+        String name=(String) session.getAttribute("name");
+
+        if (id!=null && name!=null) {
+            model.addAttribute("name", name);
+            model.addAttribute("id", id);
+            logger.info("id : "+id+" "+"name : "+name);
+        }
+        return "Main";
+    }
+
+    @GetMapping("/health") // AWS 로드 밸런서 헬스체크 경로
+    public int health() {
+        return 200;
+    }
+
+    // 로그인 여부를 확인해서 로그인이 된 상태에서만 마이페이지로 이동하게하는 코드
+    // response가 response.json을 반환하면 로그인이 된것
+    @GetMapping("/api/check-login-status")
+    public ResponseEntity<Map<String, Boolean>> checkLoginStatus(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        boolean loggedIn = (session != null && session.getAttribute("user") != null);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("loggedIn", loggedIn);
+        return ResponseEntity.ok(response);
+    }
+}
