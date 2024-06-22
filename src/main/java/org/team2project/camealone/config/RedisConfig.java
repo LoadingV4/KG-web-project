3cbf5dc534bf8b9126afccd679667f27aaf0f53a
shn07082 <127087415+shn07082@users.noreply.github.com>
2024-06-22 20:27:48 +0900

    Add files via upload

---
+++ src/main/java/org/team2project/camealone/config/RedisConfig.java
@@ -0,0 +1,18 @@
+package org.team2project.camealone.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("redis-cluster-ro.ybqofh.ng.0001.apn2.cache.amazonaws.com", 6379);
+        //return new LettuceConnectionFactory("redis1.tfcivz.ng.0001.apn2.cache.amazonaws.com", 6379);
+    }
+}
