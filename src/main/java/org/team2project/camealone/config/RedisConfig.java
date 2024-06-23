edcff58a45e91345e82e8cdf91564bd00ca37891
LoadingV4 <89256027+LoadingV4@users.noreply.github.com>
2024-06-23 23:50:19 +0900

    Update RedisConfig.java

--- src/main/java/org/team2project/camealone/config/RedisConfig.java
+++ src/main/java/org/team2project/camealone/config/RedisConfig.java
@@ -1,26 +1,28 @@
-import org.springframework.cache.annotation.EnableCaching;
+package org.team2project.camealone.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import java.util.Arrays;
 
 @Configuration
-@EnableCaching
+@EnableRedisHttpSession
 public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        // Redis 엔드포인트 주소와 포트를 하드코딩하여 설정
-        return new LettuceConnectionFactory("redis-cluster-ro.ybqofh.ng.0001.apn2.cache.amazonaws.com", 6379);
-    }
+        // 클러스터 구성 설정
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration(
+            Arrays.asList("redis-cluster-ro.ybqofh.ng.0001.apn2.cache.amazonaws.com:6379")
+        );
 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
-        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
-        return template;
+        // 필요에 따라 비밀번호 설정 (비밀번호가 없다면 주석 처리)
+        // clusterConfiguration.setPassword("your_redis_password");
+
+        return new LettuceConnectionFactory(clusterConfiguration);
     }
 }
