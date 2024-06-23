c28f6df4244d233e44f6312a0eeff882fa5456c2
LoadingV4 <89256027+LoadingV4@users.noreply.github.com>
2024-06-23 23:54:20 +0900

    Update RedisConfig.java

--- src/main/java/org/team2project/camealone/config/RedisConfig.java
+++ src/main/java/org/team2project/camealone/config/RedisConfig.java
@@ -2,12 +2,10 @@ package org.team2project.camealone.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableRedisHttpSession
@@ -15,14 +13,13 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        // 클러스터 구성 설정
-        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration(
-            Arrays.asList("redis-cluster-ro.ybqofh.ng.0001.apn2.cache.amazonaws.com:6379")
+        RedisStandaloneConfiguration standaloneConfig = new RedisStandaloneConfiguration(
+            "redis-cluster-ro.ybqofh.ng.0001.apn2.cache.amazonaws.com", 6379
         );
+        // 필요시 비밀번호 설정
+        // standaloneConfig.setPassword("your_redis_password");
 
-        // 필요에 따라 비밀번호 설정 (비밀번호가 없다면 주석 처리)
-        // clusterConfiguration.setPassword("your_redis_password");
-
-        return new LettuceConnectionFactory(clusterConfiguration);
+        return new LettuceConnectionFactory(standaloneConfig);
     }
 }
+
