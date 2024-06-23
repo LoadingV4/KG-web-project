bed1eef096bfa239c960cb1b4cc3d753557fd58c
shn07082 <127087415+shn07082@users.noreply.github.com>
2024-06-24 00:20:13 +0900

    Update RedisConfig.java

--- src/main/java/org/team2project/camealone/config/RedisConfig.java
+++ src/main/java/org/team2project/camealone/config/RedisConfig.java
@@ -1,10 +1,7 @@
-package org.team2project.camealone.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
@@ -13,13 +10,6 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration standaloneConfig = new RedisStandaloneConfiguration(
-            "redis-cluster.ybqofh.ng.0001.apn2.cache.amazonaws.com", 6379
-        );
-        // 필요시 비밀번호 설정
-        // standaloneConfig.setPassword("your_redis_password");
-
-        return new LettuceConnectionFactory(standaloneConfig);
+        return new LettuceConnectionFactory("redis-cluster-ro.ybqofh.ng.0001.apn2.cache.amazonaws.com", 6379);
     }
 }
-
