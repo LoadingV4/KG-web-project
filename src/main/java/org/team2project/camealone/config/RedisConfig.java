f2361c6abc34d31c376f7655b89c105235df20b7
LoadingV4 <89256027+LoadingV4@users.noreply.github.com>
2024-06-24 00:23:37 +0900

    Update RedisConfig.java

--- src/main/java/org/team2project/camealone/config/RedisConfig.java
+++ src/main/java/org/team2project/camealone/config/RedisConfig.java
@@ -1,3 +1,5 @@
+package org.team2project.camealone.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
