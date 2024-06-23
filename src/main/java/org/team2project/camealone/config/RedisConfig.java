60403021e6a647611b706947893fdf0e95c7d73a
LoadingV4 <89256027+LoadingV4@users.noreply.github.com>
2024-06-24 00:41:45 +0900

    Update RedisConfig.java

--- src/main/java/org/team2project/camealone/config/RedisConfig.java
+++ src/main/java/org/team2project/camealone/config/RedisConfig.java
@@ -12,6 +12,6 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("redis-cluster-ro.ybqofh.ng.0001.apn2.cache.amazonaws.com", 6379);
+        return new LettuceConnectionFactory("redis-cluster.ybqofh.ng.0001.apn2.cache.amazonaws.com", 6379);
     }
 }
